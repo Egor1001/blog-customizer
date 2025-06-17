b6a2ccd9e122470d58ac39977a86ea68779fe021
Egor_Troickiy <e.erohin2017@yandex.ru>
2025-06-18 00:46:53 +0700

    Blog

---
+++ src/components/select/hooks/useOutsideClickClose.ts
@@ -0,0 +1,31 @@
+import { useEffect } from 'react';
+
+type UseOutsideClickClose = {
+	isOpen: boolean;
+	onChange: (newValue: boolean) => void;
+	onClose?: () => void;
+	rootRef: React.RefObject<HTMLDivElement>;
+};
+
+export const useOutsideClickClose = ({
+	isOpen,
+	rootRef,
+	onClose,
+	onChange,
+}: UseOutsideClickClose) => {
+	useEffect(() => {
+		const handleMousedown = (evt: MouseEvent) => {
+			const { target } = evt;
+			if (target instanceof Node && !rootRef.current?.contains(target)) {
+				isOpen && onClose?.();
+				onChange?.(false);
+			}
+		};
+
+		window.addEventListener('mousedown', handleMousedown);
+
+		return () => {
+			window.removeEventListener('mousedown', handleMousedown);
+		};
+	}, [onClose, onChange, isOpen]);
+};
